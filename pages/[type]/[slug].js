349153264071e0c321b10e04cde8522f2c1ee51b
Willian Rodrigues <w.almeida.w@gmail.com>
2022-09-15 18:59:03 -0300

    Fixes

--- pages/[type]/[slug].js
+++ pages/[type]/[slug].js
@@ -18,16 +18,16 @@ function Block({ data, isRoot = false, extra = 0 }){
     return (<section className={ styles.block }>
         <header>
             { isRoot && <div className={ styles.tag }>{ data.type }</div> }
-            { isRoot && <h1 className={ styles.title }>{ data.name || "Sem nome"}</h1> }
-            { !isRoot && <h2 className={ styles.title }>{ data.name || "Sem nome"}</h2> }
+            { isRoot && <h1 id={ data._id } className={ styles.title }>{ data.name || "Sem nome"}</h1> }
+            { !isRoot && <h2 id={ data._id } className={ styles.title }>{ data.name || "Sem nome"}</h2> }
             <div className={ styles.value }>Custo { isRoot ? data.value : data.value > 0 ? `+${ data.value }` : data.value }</div>
         </header>
         <article className={ styles.description }><PortableText value={ data.description || {} } /></article>
         { data.rules && <section className={ styles.list }>
             <hr />
-            { data.rules.map( sub => <Block data={ sub } extra={ data.value } /> ) }
+            { data.rules.map( sub => <Block key={ sub._id } data={ sub } extra={ data.value } /> ) }
         </section> }
-        { data.origin && <footer className={ styles.footer }>{ data.origin.map( item => <div>{ item.source } p.{ item.page }</div>) }</footer> }
+        { data.origin && <footer className={ styles.footer }>{ data.origin.map( item => <div key={ item.source + item.page }>{ item.source } p.{ item.page }</div>) }</footer> }
     </section>)
 }
 
