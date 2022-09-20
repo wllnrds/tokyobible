7aca1bfe9b8857d71bdb3e792451a211da10e82a
Willian Rodrigues <w.almeida.w@gmail.com>
2022-09-19 23:42:28 -0300

    GA Tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
-import '../styles/globals.scss'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import Head from 'next/head'
 
+import '../styles/globals.scss'
+
 function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page)
-  return <>
-    <Head>
-      <title>Biblioteca de Tóquia</title>
-      <meta name="description" content="3D&T Companion" />
-      <link rel="icon" href="/favicon.ico" />
-      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
-    </Head>
-    {getLayout(<Component {...pageProps} />)}
-  </>
+    const getLayout = Component.getLayout || ((page) => page)
+    return <>
+        <GoogleAnalytics trackPageViews />
+        <Head>
+            <title>Biblioteca de Tóquia</title>
+            <meta name="description" content="3D&T Companion" />
+            <link rel="icon" href="/favicon.ico" />
+            <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+        </Head>
+        {getLayout(<Component {...pageProps} />)}
+    </>
 }
 
 export default MyApp
