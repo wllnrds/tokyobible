f73f83cf2d0a8430118d57b67ed142b7aa3e5d00
Willian Rodrigues <w.almeida.w@gmail.com>
2022-09-19 23:10:21 -0300

    Analitics Change

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
 import '../styles/globals.scss'
 import Head from 'next/head'
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
   return <>
     <Head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QXSKKLN5JW"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-QXSKKLN5JW'); </script>
       <title>Biblioteca de Tóquia</title>
       <meta name="description" content="3D&T Companion" />
       <link rel="icon" href="/favicon.ico" />
-      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'/>
+      <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
     </Head>
-    <GoogleAnalytics trackPageViews />
-    { getLayout(<Component {...pageProps} />) }
+    {getLayout(<Component {...pageProps} />)}
   </>
 }
 
