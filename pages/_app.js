073ea1c9d2b24f95e8c63646213f15942c39f7d4
Willian Rodrigues <w.almeida.w@gmail.com>
2022-09-19 23:12:21 -0300

    script fixed

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,13 @@ function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
   return <>
     <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QXSKKLN5JW"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-QXSKKLN5JW'); </script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-QXSKKLN5JW"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){ dataLayer.push(arguments) }
+        gtag('js', new Date());
+        gtag('config', 'G-QXSKKLN5JW');
+      </script>
       <title>Biblioteca de Tóquia</title>
       <meta name="description" content="3D&T Companion" />
       <link rel="icon" href="/favicon.ico" />
